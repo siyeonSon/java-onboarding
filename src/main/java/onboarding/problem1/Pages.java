0b09876cc5263804a17e6e3de84db1cd6f6ec041
siyeonSon <siyeon0209@naver.com>
2023-09-17 21:13:47 +0900

    feat(problem1): 범위 내의 페이지이다

--- src/main/java/onboarding/problem1/Pages.java
+++ src/main/java/onboarding/problem1/Pages.java
@@ -11,6 +11,8 @@ public class Pages {
         int left = numbers.get(0);
         int right = numbers.get(1);
 
+        isValidate(left, right);
+
         pages.add(new Page(left));
         pages.add(new Page(right));
     }
@@ -19,5 +21,15 @@ public class Pages {
         Page left = pages.get(0);
         Page right = pages.get(1);
         return Math.max(left.calculate(), right.calculate());
+    }
+
+    private void isValidate(int left, int right) {
+        isInRange(left, right);
+    }
+
+    private void isInRange(int left, int right) {
+        if (left < 1 || right > 400) {
+            throw new IllegalArgumentException("페이지 범위를 벗어났습니다");
+        }
     }
 }
--- src/test/java/onboarding/problem1/PagesTest.java
+++ src/test/java/onboarding/problem1/PagesTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 class PagesTest {
     @Test
@@ -12,5 +13,13 @@ class PagesTest {
         List<Integer> numbers = List.of(123, 124);
         int point = new Pages(numbers).calculatePoint();
         assertThat(point).isEqualTo(8);
+    }
+
+    @Test
+    void 범위_내의_페이지이다() {
+        List<Integer> numbers = List.of(-1, 0);
+        Exception e = assertThrows(IllegalArgumentException.class, () ->
+                new Pages(numbers));
+        assertEquals(e.getMessage(), "페이지 범위를 벗어났습니다");
     }
 }
