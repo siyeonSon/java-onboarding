eec7b9618e6115fa2286c02ad1c670efae99aaff
siyeonSon <siyeon0209@naver.com>
2023-09-20 09:42:37 +0900

    feat(problem5): 결과 반환

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,14 @@
 package onboarding;
 
+import onboarding.problem5.Money;
+
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        answer = Money.exchange(money);
         return answer;
     }
 }
